7130760c143bed4cab16576d093b2081ffb07f0f
Afema-Mark <afemamark@gmail.com>
2022-09-10 08:51:25 -0700

    corrections

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 
 /**
- *  *main - The puts command prints outputs differently from the printf command
- *   *Return: The puts command prints outputs with a new line
- *    */
+ *  main - The puts command prints outputs differently from the printf command
+ *  Return: The puts command prints outputs with a new line
+ */
 int main(void)
 {
-	    puts("\"Programming is like building a multilingual puzzle");
-
-	        return (0);
+    puts("\"Programming is like building a multilingual puzzle");                                                                                    
+    return (0);
 }
