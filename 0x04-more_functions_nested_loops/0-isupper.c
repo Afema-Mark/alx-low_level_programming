8e180bd7afcb3316fd663b9173bda64eafabbde9
Afema-Mark <afemamark@gmail.com>
2022-09-15 14:26:04 -0700

    checking the upper case aplhabets

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isupper -> this is a function to check uppercase alphabets
+ * @c: an integer parameter
+ * Return: something but success
+ */
+int _isupper(int c);
+{
+
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	retun(0);
+
+	return (c >= 65 && c <= 90);
+	return (c >= 'A' && c <= 'Z');
+}
