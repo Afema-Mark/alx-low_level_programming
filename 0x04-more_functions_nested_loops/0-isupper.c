d1371e00514bcb333722b3a2fad782654b861f8e
Afema-Mark <afemamark@gmail.com>
2022-09-15 14:49:45 -0700

    corrections

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -5,12 +5,12 @@
  * @c: an integer parameter
  * Return: something but success
  */
-int _isupper(int c);
+int _isupper(int c)
 {
 
 	if (c >= 'A' && c <= 'Z')
 		return (1);
-	retun(0);
+	retun (0);
 
 	return (c >= 65 && c <= 90);
 	return (c >= 'A' && c <= 'Z');
