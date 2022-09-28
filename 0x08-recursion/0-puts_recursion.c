0f7d520198e9238a00782d8bffbf01fc7b68df56
Afema-Mark <afemamark@gmail.com>
2022-09-28 21:38:35 +0000

    “corrections”

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -5,15 +5,16 @@
   * @s: string
   *
   * Return: On success 1.
-  * On error, -1 is returned, and errno is set appropriately.
   */
 void _puts_recursion(char *s)
 {
-	if (*s == 0)
+	if (*s == '\0')
 	{
 		_putchar('\n');
-		return;
 	}
-	_putchar(*s);
-	_puts_recursion(s + 1);
+	else
+	{
+		_putchar(*s[0]);
+		_puts_recursion(s + 1);
+	}
 }
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,7 +1,4 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-#include <stdio.h>
+void _print_rev_recursion(char *s)
 void _puts_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
@@ -10,8 +7,4 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
-int _putchar(char s)
-void check(*s, int start, int end, int *flag)
-void _print_rev_recursion(char *s)
-
-#endif /*MAIN_H*/
+int _putchar(char c)
