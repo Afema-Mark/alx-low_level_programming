8e9535a4f670bac4ceb924267f385412c6d9e6b7
Afema-Mark <afemamark@gmail.com>
2022-09-28 22:58:50 +0000

    “corrections”

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,18 +3,17 @@
 /**
   * _puts_recursion - Prints a string followed by a new line
   * @s: string
-  *
-  * Return: On success 1.
   */
 void _puts_recursion(char *s)
 {
-	if (*s == 0)
-	{
-		_putchar('\n');
-	}
-	else
+	if (*s)
 	{
 		_putchar(*s);
 		_puts_recursion(s + 1);
 	}
+	else
+	{
+		_putchar('\n');
+	}
+
 }
