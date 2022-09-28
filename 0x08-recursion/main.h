7a86575a0123dd53e685eea05b9ebea314569bb6
Afema-Mark <afemamark@gmail.com>
2022-09-28 20:47:31 +0000

    corrections

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -7,7 +7,7 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
-int _putchar(char c)
+int _putchar(char s)
 <<<<<<< HEAD
 void check(*s, int start, int end, int *flag)	
 =======
