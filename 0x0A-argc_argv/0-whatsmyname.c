67068a39e56684593417c133ec1b050dfaec4bfe
Afema-Mark <afemamark@gmail.com>
2022-09-30 18:38:59 +0000

    task0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+  *main - write aprogram that prints its name,it ends with a new line
+  *@argc: this is the argument count
+  *@argv: this is the argument vector
+  *Return - 0
+  */
+int main(argc,char *argv[]))
+{
+	prinf("%s\n",argv[]);
+	(void)argc;
+	return(0)
+}
