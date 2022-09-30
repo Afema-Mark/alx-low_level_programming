19531c7e1ffb6c2146d6d0cbda562f28e146c504
Afema-Mark <afemamark@gmail.com>
2022-09-30 20:09:47 +0000

    “corrections”

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,7 +4,7 @@
   *main - write aprogram that prints its name,it ends with a new line
   *@argc: this is the argument count
   *@argv: this is the argument vector
-  *Return - This return 0
+  *Return: This return 0
   */
 int main(int argc, char *argv[])
 {
