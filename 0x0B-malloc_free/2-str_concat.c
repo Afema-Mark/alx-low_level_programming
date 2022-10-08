3980e4bfd560bd7e1c7389010ed07b8e3dff0740
Afema-Mark <afemamark@gmail.com>
2022-10-08 07:18:57 +0000

    “task2”

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,51 +1,54 @@
-#include <stdlib.h>
-
-/**
- * str_concat - concatenates two strings into a newly allocated space in memory
- * @s1: First string to be concatenated
- * @s2: Second string to be concatenated after s1
- *
- * Return: A pointer to the newly allocated memory, or NULL on failure
- */
-
-char *str_concat(char *s1, char *s2)
+include "main.h" 
+ #include <stdlib.h> 
+  
+ /** 
+  * _strlen - gets the length of a string 
+  * @s: character 
+  * Return: string length 
+  */ 
+ int _strlen(char *s) 
 {
-	int i;
-	char *ptr = NULL;
-	char *as1 = s1;
-	char *as2 = s2;
-
-	if (as1 == NULL)
-		as1 = "";
-
-	if (as2 == NULL)
-		as2 = "";
-
-	ptr = as1;
-	while (*ptr++)
-		i++;
-
-	ptr = as2;
-	while (*ptr++)
-		i++;
-
-	i++;
-	ptr = malloc(i * sizeof(char));
-
-	if (ptr == NULL)
-		return (NULL);
-
-		i = 0;
-	while (*as1)
-	{
-		ptr[i++] = *as1++;
-	}
-
-	while (*as2)
-	{
-		ptr[i++] = *as2++;
-	}
-	ptr[i] = *as2;
-
-	return (ptr);
+	 int l = 0; 
+		    
+	 while (*s != '\0')
+	 {
+		s++;
+		l++; 
+	 } 
+	 return (l); 
+} 
+  
+ /** 
+  *str_concat - caoncats two strings 
+  *@s1: string one 
+  *@s2: string two 
+  *Return: conc 
+  */ 
+ char *str_concat(char *s1, char *s2) 
+{ 
+	unsigned int k1, k2; 
+	char *conc, *tmp; 
+		     
+	if (!s1) 
+		s1 = ""; 
+	else 
+		k1 = _strlen(s1); 
+	if (!s2) 
+		s2 = ""; 
+	else 
+		k2 = _strlen(s2); 
+		         
+	conc = malloc(k1 + k2 + 1); 
+	if (!conc) 
+		return (0); 
+			   
+	tmp = conc; 
+	while (*s1) 
+		*tmp++ = *s1++; 
+			     
+	while ((*tmp++ = *s2++)) 
+		; 
+			      
+	return (conc); 
 }
+
