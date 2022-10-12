dbc593128959a475d20b924958155bf20c930f38
Afema-Mark <afemamark@gmail.com>
2022-10-12 08:29:52 +0000

    “task1”

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,29 +1,20 @@
-
 #include "function_pointers.h"
-#include <stdio.h>
 #include <stdlib.h>
 /**
-* int_index - return index place if comparison = true, else -1
-* @array: array
-* @size: size of elements in array
-* @cmp: pointer to func of one of the 3 in main
-* Return: 0
-*/
-int int_index(int *array, int size, int (*cmp)(int))
+   * array_iterator - iterates an array
+    * @array: is an array
+     * @size: is the size
+      * @action: is something
+      */
+void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	int i;
+	unsigned int i;
 
 	if (array == NULL)
-		return (-1);
-	if (size <= 0)
-		return (-1);
-	if (cmp == NULL)
-		return (-1);
+		return;
+	if (action == NULL)
+		return;
 
 	for (i = 0; i < size; i++)
-	{
-		if (cmp(array[i]) != 0)
-			return (i);
-	}
-	return (-1);
+		action(array[i]);
 }
